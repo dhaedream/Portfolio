165ae63acb7d1d9cd36447ffcbb15b8b4faf0d98
paintincode <dhaesha1@gmail.com>
2021-10-29 03:15:22 -0400

    replaced x button css with class identifier to correct navs visibility issue. pop up feature coming next

--- actions.js
+++ actions.js
@@ -1,6 +1,7 @@
 function managePopMenu() {
-    navEle = document.querySelector("#openMobileNav")
-    smNav = document.querySelector(".mobile-closed-menu")
-    navEle.style.display = "none"
-    smNav.style.borderTop = "1px solid grey"
+    navEle = document.querySelector("#bigMenu")
+    smNav = document.querySelector(".#smMenu")
+    xBut = document.querySelector("#exitMenu")
+    // navEle.style.display = "none"
+    // smNav.style.borderTop = "1px solid grey"
 }
