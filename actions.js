d5890eb38c5c9e2c9a7ae543a9fd5d6ada46b535
paintincode <dhaesha1@gmail.com>
2021-11-11 00:29:32 -0500

    h1 tags positioned at top of div

--- actions.js
+++ actions.js
@@ -6,6 +6,7 @@ var xBut = document.querySelector("#exitMenu")
 hamburger.addEventListener("click", function() {
     bigNav.style.display = "flex"
     smNav.style.display = "none"
+    // document.body.style.height = "100px";
 }, false);
 
 xBut.addEventListener("click", function() {
