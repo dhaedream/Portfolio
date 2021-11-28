2a1ac6877047cb1ae56ee020da959606afd7aed3
paintincode <dhaesha1@gmail.com>
2021-11-28 17:32:55 -0500

    working on menu pop up

--- actions.js
+++ actions.js
@@ -1,12 +1,27 @@
+var webPage = document.querySelector('body')
+
 var bigNav = document.querySelector("#bigMenu")
 var hamburger = document.querySelector("#hamb");
 var smNav = document.querySelector("#smMenu")
 var xBut = document.querySelector("#exitMenu")
 
 hamburger.addEventListener("click", function() {
-    bigNav.style.display = "flex"
-    smNav.style.display = "none"
+    // switch from small to large menu
+    bigNav.style.display = "flex";
+    smNav.style.display = "none";
+
+    // make fullscreen
+    document.body.style.position = "relative";
+    bigNav.style.position = "absolute";
+    bigNav.style.top = "0px";
+    bigNav.style.right = "0px";
+
+    // webPage.style.height = "100vh";
+    // webPage.style.overflowX = "hidden";
     // document.body.style.height = "100px";
+    document.body.style.overflowY = "hidden";
+    // document.body.style.height = "100vh";
+
 }, false);
 
 xBut.addEventListener("click", function() {
