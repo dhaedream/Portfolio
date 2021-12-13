8f5429a5e33bf920fc34b9a9950ceae51aea5563
paintincode <dhaesha1@gmail.com>
2021-12-13 00:17:19 -0500

    menu links added

--- actions.js
+++ actions.js
@@ -3,42 +3,13 @@ var bigNav = document.querySelector("#bigMenu")
 var hamburger = document.querySelector("#hamb");
 var smNav = document.querySelector("#smMenu")
 var xBut = document.querySelector("#exitMenu")
-// var homeDiv = document.querySelector(".home")
-// var headers = document.querySelector("header")
-// var bioDiv = document.querySelector(".bio")
-// var projectDiv = document.querySelector(".projects")
-// var headerTwo = document.querySelector("h2")
-// var skillDiv = document.querySelector(".skills")
-// var resumeDiv = document.querySelector(".resume")
-// var contactDiv = document.querySelector(".contact")
-// var conHead = document.querySelector(".con-head")
-// var thx = document.querySelector(".thank-you")
+var navLinks = document.querySelectorAll(".nav-tab")
 
 hamburger.addEventListener("click", function() {
     // switch from small to large menu
     bigNav.style.display = "flex";
     smNav.style.display = "none";
-//     // make fullscreen
-//     document.body.style.display = "block";
-//     document.body.style.position = "relative";
-//     bigNav.style.position = "absolute";
-//     bigNav.style.top = "0px";
-//     bigNav.style.right = "0px";
-//     // hide ENTIRE webpage "behind" menu
-//     bigNav.style.backgroundImage = "url('media/images/background.png')";
-//     homeDiv.style.display = "none";
-//     homeDiv.style.display = "none";
-//     headers.style.display = "none";
-//     projectDiv.style.display = "none";
-//     bioDiv.style.display = "none";
-//     headerTwo.style.display = "none";
-//     skillDiv.style.display = "none";
-//     resumeDiv.style.display = "none";
-//     contactDiv.style.display = "none";
-//     thx.style.display = "none";
-//     conHead.style.display = "none";
-
-//     // disable scrolling
+    // disable scrolling
     document.body.style.overflowY = "hidden";
 }, false);
 
@@ -50,3 +21,11 @@ xBut.addEventListener("click", function() {
     document.body.style.overflowY = "visible";
 
 }, false);
+
+for (i of navLinks) {
+    i.addEventListener("click", function() {
+        bigNav.style.display = "none"
+        smNav.style.display = "flex"
+        document.body.style.overflowY = "visible";
+    }, false);
+}
